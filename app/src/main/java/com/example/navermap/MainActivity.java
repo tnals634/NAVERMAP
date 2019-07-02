6be9aa86b7bf9ac9692ee490d1dca46b07abde68
tnals634 <tnals634@naver.com>
2019-07-02 15:15:23 +0900

    naver map 군산대학교 좌표에 marker의 캡션 색상

--- app/src/main/java/com/example/navermap/MainActivity.java
+++ app/src/main/java/com/example/navermap/MainActivity.java
@@ -142,10 +142,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //marker.setAngle(90); // 각도 조정
         //marker.setFlat(true); // 눕힘
         //marker.setIconPerspectiveEnabled(true); //원근 효과
+
         marker.setCaptionText("Hello"); //캡션 택스트 설정
         //marker.setCaptionRequestedWidth(200); // 텍스트 줄바꿈
-        marker.setCaptionAlign(Align.Top); //캡션 위치 설정 (위에 배치
-        marker.setCaptionOffset(30);//아이콘과 켑션 사이 30픽셀의 간격 지정
+        //marker.setCaptionAlign(Align.Top); //캡션 위치 설정 (위에 배치
+        //marker.setCaptionOffset(30);//아이콘과 켑션 사이 30픽셀의 간격 지정
+        marker.setCaptionColor(Color.BLUE); //캡션을 파란색으로 지정
+        marker.setCaptionHaloColor(Color.rgb(200,255,200)); //캡션 외곽선을 연한 녹색으로 지정
+
         marker.setMap(naverMap);
 
 
